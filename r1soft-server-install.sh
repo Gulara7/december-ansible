cf8a7708181e47afa8f56c2cb3d58c04066f9198
farrukh90 <farrukhsadykov@gmail.com>
2022-02-24 01:35:03 +0000

    added r1soft

---
+++ r1soft-server-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo deb http://repo.r1soft.com/apt stable main >> /etc/apt/sources.list
+wget http://repo.r1soft.com/r1soft.asc
+sudo apt-key add r1soft.asc 
+sudo apt-get update 
+sudo apt-get install serverbackup-enterprise  -y
+sudo serverbackup-setup --user root --pass    redhat  
+sudo serverbackup-setup --http-port 80 --https-port 443 
+sudo systemctl restart sbm-server.service
+systemctl status sbm-server.service
