868d0b42472af03c2be12a110f086c9badeb5d79
farrukh90 <farrukhsadykov@gmail.com>
2022-02-26 16:15:23 +0000

    added version

--- localscript.sh
+++ localscript.sh
@@ -1,4 +1,4 @@
-ansible-galaxy collection install amazon.aws
+ansible-galaxy collection install amazon.aws:0.1.1
 ansible-galaxy collection install community.aws
 sudo yum install python-pip -y
 pip install boto botocore boto3
